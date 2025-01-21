c504890a0ae3c626cd86bd1369faf913223e7383
Utwo <mihai.legat@gmail.com>
2025-01-21 16:19:26 +0200

    Add service version in / response

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -61,7 +61,7 @@ io.of((_name, _auth, next) => {
 });
 
 app.post("/get-file-content", getFileContentReq);
-app.get("/", ({ text }) => text(`ok, ${config.serviceVersion}`));
+app.get("/", ({ text }) => text("ok"));
 
 // delete pods that are not in active rooms
 const cleanupInterval = setInterval(async () => {
